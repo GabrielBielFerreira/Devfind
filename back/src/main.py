39f2d9513ea12f615598fa4ca9b586bbae8dbf49
Mikael Silva <29325104-mikaelsl@users.noreply.gitlab.com>
2025-12-10 21:26:14 -0300

    feat: mudar para Flask

--- back/src/main.py
+++ back/src/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from back!")
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
-if __name__ == "__main__":
-    main()
+@app.route("/api/profile/<int:id>")
+def get_profile(id: int):
+    # Dados temporários
+    name = "Ana Maria"
+    description = "Desenvolvedora Front-End Júnior"
+    location = "São Paulo - SP"
+    about = "Sou desenvolvedora com experiência em criação de aplicações modernas e escaláveis, sempre focado em oferecer soluções eficientes e bem estruturadas. Tenho facilidade em trabalhar em equipe, adaptar-me a novos desafios e aprender novas tecnologias rapidamente. Nos últimos anos, atuei em projetos que envolveram interfaces responsivas, integrações com APIs e boas práticas de performance e acessibilidade."
+    skills = ["CSS", "HTML", "JavaScript", "React"]
+    experience = [{"name": "Empresa A", "role": "Desenvolvedora Front-End"}]
+    return {
+        "name": name,
+        "description": description,
+        "location": location,
+        "about": about,
+        "skills": skills,
+        "experience": experience,
+    }
--- front/src/pages/profile/profilePage.tsx
+++ front/src/pages/profile/profilePage.tsx
@@ -1,33 +1,45 @@
 import ProfilePageStyled from "./profilePageStyled"
 import ExperienceCard from "../../components/experienceCard/experienceCard"
+import React from "react";
 
 export const ProfilePage = () => {
+    const [data, setData] = React.useState(null);
+
+    const loadData = async () => {
+        const url = 'http://127.0.0.1:5000/api/profile/0';
+        const options = { method: 'GET' };
+        try {
+            const response = await fetch(url, options);
+            setData(await response.json());
+        } catch (error) {
+            console.error(error);
+            setData(null);
+        }
+    }
+
+    React.useEffect(() => {
+        loadData();
+    }, []);
 
     return (
         <ProfilePageStyled>
-            
+
             <section className="profileDetail">
                 <h1>Perfil</h1>
 
                 <div className="mainDetail">
                     <img src="https://i.etsystatic.com/31548528/r/il/ffde13/5804742914/il_300x300.5804742914_ap2d.jpg" alt="Imagem de Perfil" />
                     <section className="text">
-                        <h2>Ana Maria</h2>
-                        <p>Desenvolvedora Front-End <span>Júnior</span></p>
-                        <p className="city">São Paulo - SP</p>
+                        <h2>{data ? data.name : 'Loading name...'}</h2>
+                        <p>{data ? data.description : 'Loading description...'}</p>
+                        <p className="city">{data ? data.city : 'Loading city...'}</p>
                         <button>Editar Perfil</button>
                     </section>
-                    
+
                 </div>
                 <section className="about">
                     <h3>Sobre</h3>
-                    <p>
-                        Sou desenvolvedor com experiência em criação de aplicações modernas e escaláveis, 
-                        sempre focado em oferecer soluções eficientes e bem estruturadas. Tenho facilidade 
-                        em trabalhar em equipe, adaptar-me a novos desafios e aprender novas tecnologias 
-                        rapidamente. Nos últimos anos, atuei em projetos que envolveram interfaces responsivas, 
-                        integrações com APIs e boas práticas de performance e acessibilidade.
-                    </p>
+                    <p>{data ? data.about : 'Loading about....'}</p>
                 </section>
                 <section className="skil">
                     <h3>Habilidades</h3>
@@ -50,14 +62,14 @@ export const ProfilePage = () => {
             </section>
             <section className="experience">
                 <h2>Experiencias</h2>
-                
+
                 <div className="companies">
                     <ul>
                         <li>
-                            <ExperienceCard/>
+                            <ExperienceCard />
                         </li>
                         <li>
-                            <ExperienceCard/>
+                            <ExperienceCard />
                         </li>
                     </ul>
                 </div>
@@ -66,11 +78,11 @@ export const ProfilePage = () => {
                 <div className="companies">
                     <ul>
                         <li>
-                            <ExperienceCard/>
+                            <ExperienceCard />
                         </li>
-                        
-                        
-                     
+
+
+
                     </ul>
                 </div>
             </section>
